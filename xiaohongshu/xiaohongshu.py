a9eb5d85c8afd8b3102e15b31a44476e67e51b9a
Li huimin <362169885@qq.com>
2019-07-17 15:06:07 +0800

    push

--- xiaohongshu/xiaohongshu.py
+++ xiaohongshu/xiaohongshu.py
@@ -5,11 +5,12 @@ import requests
 url = 'https://www.xiaohongshu.com/wx_mp_api/sns/v1/homefeed?oid=recommend&page=25&page_size=20'
 
 headers = {
-'upgrade-insecure-requests': '1',
-'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
 }
 
-result =requests.get(url=url,headers=headers)
-datas = json.loads(result.text)['data']
-for data in datas:
-    print(data['id'])
+result = requests.get(url=url, headers=headers)
+print(result.text)
+# datas = json.loads(result.text)['data']
+# for data in datas:
+#     print(data['id'])
