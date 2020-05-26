ea7fdee227d455eea9695feb8073f636e9e84e17
Boomshakal <31717950+Boomshakal@users.noreply.github.com>
2020-05-26 11:18:22 +0800

    add set_window_size

--- PhantomJS_test/test.py
+++ PhantomJS_test/test.py
@@ -10,8 +10,10 @@ import time
 
 print("start....\n")
 driver = webdriver.PhantomJS()
-url = 'http://192.168.151.140:8075/WebReport/ReportServer?reportlet=ikahe_kanban%2Fxqhz.cpt'
+driver.set_window_size(1400, 700)
+url = 'http://192.168.151.140:8075/WebReport/ReportServer?reportlet=ikahe_kanban%2F%5B7ef4%5D%5B4fee%5D%5B62a5%5D%5B8868%5D.cpt'
 res = driver.get(url)
 print(res)
-# driver.save_screenshot("kanban.jpeg")
+time.sleep(10)
+driver.save_screenshot(r"D:\桌面\repair.png")
 print("ok!\n")
