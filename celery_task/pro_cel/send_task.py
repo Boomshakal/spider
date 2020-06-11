46f6329abdce4e8ae6789fdb33a93ed4e9636e92
Lihuimin <362169885@qq.com>
2020-06-11 18:47:05 +0800

    Update send_task.py

--- celery_task/pro_cel/send_task.py
+++ celery_task/pro_cel/send_task.py
@@ -6,3 +6,9 @@ result = test_celery.delay('第一个的执行')
 print(result.id)
 result = test_celery2.delay('第二个的执行')
 print(result.id)
+
+tp = time.time()
+utc_time = datetime.datetime.utcfromtimestamp(tp)
+add_time = datetime.timedelta(seconds=10)
+utc_time = utc_time + add_time
+result = test_celery.apply_async(args=('定时任务'), eta=utc_time)
